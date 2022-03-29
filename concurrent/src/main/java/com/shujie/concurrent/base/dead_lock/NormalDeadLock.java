b743961ec2c82f876cbbd8a3bb47b572855b1eeb
linshujie <1010368787@qq.com>
2022-03-29 09:38:29 +0800

    1.修改配置 2.添加死锁类 3.添加线程池类

---
+++ concurrent/src/main/java/com/shujie/concurrent/base/dead_lock/NormalDeadLock.java
@@ -0,0 +1,61 @@
+package com.shujie.concurrent.base.dead_lock;
+
+/**
+ * File description
+ *
+ * @author linshujie
+ * @date 2022/3/28
+ */
+public class NormalDeadLock {
+    private static Object lock1 = new Object();
+    private static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+        thread1.start();
+        thread2.start();
+    }
+
+    private static class Thread1 extends Thread{
+        @Override
+        public void run() {
+            try {
+                doSomeThing1();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void doSomeThing1() throws InterruptedException {
+            synchronized (lock1){
+                System.out.println(Thread.currentThread().getName() + " get lock1 ");
+                Thread.sleep(100);
+                synchronized (lock2){
+                    System.out.println(Thread.currentThread().getName() + " get lock2");
+                }
+            }
+        }
+    }
+
+    private static class Thread2 extends Thread{
+        @Override
+        public void run() {
+            try {
+                doSomeThing2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void doSomeThing2() throws InterruptedException {
+            synchronized (lock1){
+                System.out.println(Thread.currentThread().getName() + " get lock1 ");
+                Thread.sleep(100);
+                synchronized (lock2){
+                    System.out.println(Thread.currentThread().getName() + " get lock2");
+                }
+            }
+        }
+    }
+}
